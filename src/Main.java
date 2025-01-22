24e33a698966b5f184e48e4e7c8f2ca56f417c21
Anastasia3012 <dolgopolova3012@mail.ru>
2025-01-22 17:07:42 +1000

    выполненное ДЗ

--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,37 @@ public class Main {
         friend = friend / 7;
         System.out.println(friend);
 
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
 
+        var weight1 = 78.2;
+        System.out.println(weight1);
+        var weight2 = 82.7;
+        System.out.println(weight2);
+        var weightGeneral = weight1 + weight2;
+        System.out.println(weightGeneral);
+        var weightDifference = weight1 - weight2;
+        System.out.println(weightDifference);
+        var overload = weight2 % weight1;
+        System.out.println(overload);
+
+        var hours = 640;
+        System.out.println(hours);
+        var workindDay = 8;
+        System.out.println(workindDay);
+        var workers = hours / workindDay;
+        System.out.println("Всего работников в компании - " + workers + " человек.");
+
+        workers = workers + 94;
+        System.out.println(workers);
+        hours = workers * workindDay;
+        System.out.println("Если в компании работает " + workers + " человека, то всего " + hours + " часов работы может быть поделено между сотрудниками.");
 
 
     }
